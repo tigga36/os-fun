8a6a50c9e7d359ea3e4d3b5982d981ee66a36f48
Taiga <onotaiga36@gmail.com>
2020-08-28 21:11:48 +0900

    Implement boot loader to load kernel in 32-bit protected mode

--- kernel/boot_sect.asm
+++ kernel/boot_sect.asm
@@ -24,3 +24,38 @@ KERNEL_OFFSET equ 0x1000 ; Memory offset to where to load the kernel
 %include "../boot_loader/protected/gdt.asm"
 %include "../boot_loader/protected/print_string_pm.asm"
 %include "../boot_loader/protected/switcheroo.asm"
+
+[bits 16]
+
+; Method to load kernel
+load_kernel:
+	mov bx, MSG_LOAD_KERNEL ; Let the world know
+	call print_string
+	
+	mov bx, KERNEL_OFFSET ; Setting up arguments in registers to get ready for drive_reader
+	mov dh, 15 ; The 15 here means that basically we read the first 15 sectors of the boot disk 
+	mov dl, [BOOT_DRIVE]
+	call disk_load
+
+	ret
+
+[bits 32]
+; Arrived to protected mode, and begin setup of PM environment
+
+BEGIN_PM:
+	mov ebx, MSG_PROT_MODE ; Let the world know we are in business
+	call print_string_pm
+
+	call KERNEL_OFFSET ; Jump to address of where the kernel code should be loaded
+
+	jmp $
+
+; Global Variables
+BOOT_DRIVE db 0
+MSG_REAL_MODE db "We are in 16-bit mode like the scrubs we are", 0
+MSG_PROT_MODE db "WHY YES HELLO IT IS I THE CPU IN THE 32-BIT SYSTEM", 0
+MSG_LOAD_KERNEL db "GOOD EVENING AND THIS IS MY KERNEL", 0
+
+; Bootsector usual padding
+times 510-($-$$) db 0
+dw 0xaa55
