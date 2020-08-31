14eacd36bd164f9c7a025699e30cc6314ba3a172
Taiga <onotaiga36@gmail.com>
2020-08-31 20:12:44 +0900

    Initialize with basic global variables

---
+++ drive/screen.h
@@ -0,0 +1,10 @@
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+
+// Byte to set color scheme
+#define WHITE_ON_BLACK 0x0f
+
+// I/O ports for screen device
+#define REG_SCREEN_CTRL 0x3D4
+#define REG_SCREEN_DATA 0x3D5
