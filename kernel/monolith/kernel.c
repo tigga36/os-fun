dc8485b9379804fba7009cd8a005fc1ab69250e0
Taiga <onotaiga36@gmail.com>
2020-09-15 21:47:21 +0900

    Implement scrolling

--- kernel/monolith/kernel.c
+++ kernel/monolith/kernel.c
@@ -91,15 +91,31 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 void terminal_putchar(char c)
 {
 	if(c=='\n'){
-		terminal_column = 0;
-		terminal_row++;
+		terminal_column = VGA_WIDTH - 1;
 	} else{
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-		if(++terminal_column == VGA_WIDTH){
-			terminal_column = 0;
-			if(++terminal_row == VGA_HEIGHT){
-				terminal_row = 0;
+	}
+	if(++terminal_column == VGA_WIDTH){
+		terminal_column = 0;
+		// NOTE: The below conditional may cause undefined behaviour due to the fact that terminal_row can infinitely increase until integer overflow
+		if(++terminal_row >= VGA_HEIGHT){
+			// terminal_row = 0;
+			// Implement Scrolling
+			terminal_buffer = (uint16_t*) 0xB8000;
+			terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+			for(size_t y=0;y<VGA_HEIGHT-1;y++){
+				for(size_t x=0;x<VGA_WIDTH;x++){
+					const size_t index = y * VGA_WIDTH + x;
+					terminal_buffer[index] = terminal_buffer[index+VGA_WIDTH];
+				}
 			}
+			for(size_t x=0;x<VGA_WIDTH;x++){
+				const size_t index = (VGA_HEIGHT-1) * VGA_WIDTH + x;
+				terminal_buffer[index] = vga_entry(' ', terminal_color);
+			}
+			terminal_row--;
+			// All pointers to data in VGA memory should be decremented by an entire row, while the earliest VGA memory is discarded, leaving an empty row as the last row		
+				
 		}
 	}
 }
@@ -118,5 +134,6 @@ void terminal_writestring(const char* data)
 void kernel_main(void)
 {
 	terminal_initialize();
-	terminal_writestring("Hey, kernel here\nLine break too\n");
+	terminal_writestring("Apple\nBannana\nCorn\nDrive\nEeach\nFeet\nGay\nHard\nInteresting\nJousting\nKotlin\nLoud\nMorning\nNasm\nOperate\nPort\nQuestion\nRed\nSet\nTeach\nUnderstand\nVagrant\nWin\nXerox\nYard\nZeal\n");
+	// terminal_writestring("A\nB\n");
 }
